4d141296e2281af9628428d0ebf0e11061f58fc8
Random Dude <108026622+randomdude441@users.noreply.github.com>
2026-02-27 12:59:43 -0600

    Update axiscontrol.h

--- axiscontrol/axiscontrol.h
+++ axiscontrol/axiscontrol.h
@@ -8,18 +8,27 @@ class axiscontrol : public BakkesMod::Plugin::BakkesModPlugin
 {
 private:
 	bool isInGoalReplay = false;
-	bool wasSuperSonic = false;
 	std::chrono::steady_clock::time_point timeStartedBuffer;
 	std::shared_ptr<bool>  workWhileSpectating;
+	std::shared_ptr<int> logMode;
+	std::string lastExecutedCommand = "";
+	
+
+
 
 
 public:
 	//std::shared_ptr<bool>  onAlways;
 	std::shared_ptr<std::string> command1;
-	std::shared_ptr<bool> forceUpdate;
 	void onLoad() override;
 	void onUnload() override;
 	void executeCommands();
 	ServerWrapper GetCurrentGameState();
 	void Render(CanvasWrapper canvas);
+	void GetControllerInput(std::shared_ptr<CVarManagerWrapper> cvarManager);
+	std::shared_ptr<bool> forceExecuteCommand;
+	std::shared_ptr<std::string> execCommands[9]; // Array for the 9 command strings
+	std::string lastExecutedCommands[9];         // Array to track changes for each
+	std::shared_ptr<bool> showDebugInputs;
+	std::shared_ptr<int> manualControllerIndex; 
 };
